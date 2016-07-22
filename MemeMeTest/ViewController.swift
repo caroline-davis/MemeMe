476f51527196b40b9027b567b9bb4cf63fcce33f
Caroline Davis <caroline_davis@live.com>
2016-07-22 13:55:12 +1000

    first commit

---
+++ MemeMeTest/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MemeMeTest
+//
+//  Created by Caroline Davis on 22/07/2016.
+//  Copyright © 2016 Caroline Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+  
+    @IBAction func pickAnImage(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(pickerController: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(pickerController: UIImagePickerController) {
+        print("teste")
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+}
+
